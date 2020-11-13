e5d5969efba009dd299e585285e4b06a21f73073
LuoBo <aluobobo@outlook.com>
2020-11-13 11:38:43 +0800

    webView can go back and fix bugs

--- app/src/main/java/com/luobo/wanandroid/util/WebActivity.java
+++ app/src/main/java/com/luobo/wanandroid/util/WebActivity.java
@@ -2,13 +2,18 @@ package com.luobo.wanandroid.util;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import com.luobo.wanandroid.BaseActivity;
 import com.luobo.wanandroid.R;
+
+import static android.view.KeyEvent.KEYCODE_BACK;
 
 public class WebActivity extends BaseActivity {
     WebView mWebView;
@@ -17,12 +22,36 @@ public class WebActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
-        mWebView = findViewById(R.id.webView);
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        mWebView.setWebViewClient(new WebViewClient());
+        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        mWebView = new WebView(getApplicationContext());
+        addContentView(mWebView, params);
+        initWebView();
         Intent intent = getIntent();
         mWebView.loadUrl(intent.getStringExtra("URL"));
+    }
+
+    public void initWebView() {
+        mWebView.canGoBack();
+        mWebView.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setUseWideViewPort(true); //将图片调整到适合webView的大小
+        webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+        webSettings.setJavaScriptEnabled(true);//如果访问的页面中要与Javascript交互，则webView必须设置支持Javascript
+
+    }
+
+    //Back键控制网页后退
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if ((keyCode == KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+
     }
 
 
